db7ee07dd07f10a3190d4c694de56173a832c617
Adam Mescher <admescher@gmail.com>
2017-12-13 14:41:19 -0700

    Added styles

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const accordionTabsViewCodeButton = document.getElementsByClassName('accordion-tabs-view-code-button')[0];
+const flexibleCardsViewCodeButton = document.getElementsByClassName('flexible-card-view-code-button')[0];
+const headerBarViewCodeButton = document.getElementsByClassName('header-bar-view-code-button')[0];
+
+accordionTabsViewCodeButton.addEventListener('click', () => {
+  
+});
+
+flexibleCardsViewCodeButton.addEventListener('click', () => {
+  
+});
+
+headerBarViewCodeButton.addEventListener('click', () => {
+  
+});
