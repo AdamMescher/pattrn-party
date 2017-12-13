5796a877a8543b2fada7fa30860c2c76bb863b80
Adam Mescher <admescher@gmail.com>
2017-12-13 15:12:11 -0700

    Add styles

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const flexibleCardsViewCodeButton = document.getElementsByClassName('flexible-ca
 const headerBarViewCodeButton = document.getElementsByClassName('header-bar-view-code-button')[0];
 
 accordionTabsViewCodeButton.addEventListener('click', () => {
-  
+  window.open(`https://codepen.io/adammescher/pen/mpbPNe`);
 });
 
 flexibleCardsViewCodeButton.addEventListener('click', () => {
