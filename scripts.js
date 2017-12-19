f12b09540a19282d8a9351e9d962f8755e562cb0
Adam Mescher <admescher@gmail.com>
2017-12-19 12:27:08 -0700

    Add codepen visibility toggle on button press

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 const accordionTabsViewCodeButton = document.getElementsByClassName('accordion-tabs-view-code-button')[0];
 const flexibleCardsViewCodeButton = document.getElementsByClassName('flexible-card-view-code-button')[0];
 const headerBarViewCodeButton = document.getElementsByClassName('header-bar-view-code-button')[0];
+const changeButtonText = button => {
+  if(button.text() === 'view code'){
+    return button.text('hide code');
+  }
+  button.text('view code');
+};
 
 accordionTabsViewCodeButton.addEventListener('click', () => {
-  window.open(`https://codepen.io/adammescher/pen/mpbPNe`);
+  $('.codepen__embed').toggleClass('hidden');
+  changeButtonText($('.accordion-tabs-view-code-button'));
 });
 
 flexibleCardsViewCodeButton.addEventListener('click', () => {
@@ -13,3 +20,4 @@ flexibleCardsViewCodeButton.addEventListener('click', () => {
 headerBarViewCodeButton.addEventListener('click', () => {
   
 });
+
