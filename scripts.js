76c074bae9f7cb5de47046fe4def725df776d6e1
Adam Mescher <admescher@gmail.com>
2018-01-03 15:26:10 -0700

    Add header bar

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const accordionTabsViewCodeButton = document.getElementsByClassName('accordion-tabs-view-code-button')[0];
 const flexibleCardsViewCodeButton = document.getElementsByClassName('flexible-card-view-code-button')[0];
 const headerBarViewCodeButton = document.getElementsByClassName('header-bar-view-code-button')[0];
+const headerBarMenuButton = document.getElementsByClassName('header-bar__menu-button')[0];
+const headerBarNavList = document.getElementsByClassName('header-bar__nav__list')[0];
+const headerBarSearchContainer = document.getElementsByClassName('header-bar__search-container')[0];
+
 const changeButtonText = button => {
   if(button.text() === 'view code'){
     return button.text('hide code');
@@ -18,7 +22,13 @@ flexibleCardsViewCodeButton.addEventListener('click', () => {
   changeButtonText($('.flexible-card-view-code-button'));
 });
 
-headerBarViewCodeButton.addEventListener('click', () => {
-  
+headerBarViewCodeButton.addEventListener('click', function(){
+  $('.header-bar__codepen').toggleClass('hidden');
+  changeButtonText($('.flexible-card-view-code-button'));
+});
+
+headerBarMenuButton.addEventListener('click', () => {
+  $('.header-bar__nav__list').toggleClass('hidden');
+  $('.header-bar__search-container').toggleClass('hidden');
 });
 
